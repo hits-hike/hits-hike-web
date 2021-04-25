f04ae2df75e81e49c9dc6413468e9a5619a0fa03
Chorom Ham <chorom.m.ham@gmail.com>
2021-04-25 23:29:03 +0900

    ✨feat: Add components to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import GNB from "../src/components/organisms/GNB";
+import Banner from "../src/components/organisms/Banner";
+import YoutubeTitleRecommendation from "../src/components/organisms/YoutubeTitleRecommendation";
+import GFooter from "../src/components/organisms/GFooter";
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <>
       <GNB></GNB>
+      <Banner></Banner>
+      <YoutubeTitleRecommendation></YoutubeTitleRecommendation>
+      <GFooter></GFooter>
     </>
   );
 }
